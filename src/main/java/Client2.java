b689a774231b255ea8025a8cc98756aabc800972
dd77474h <dennis.diefenbach@univ-st-etienne.fr>
2016-10-21 13:54:09 +0200

    With code to input music genres

--- src/main/java/Client2.java
+++ src/main/java/Client2.java
@@ -11,15 +11,32 @@ public class Client2 {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         try {
         	
-            String[] URI = new String[5];
-            URI[0]="http://dbpedia.org/resource/Silvio_Berlusconi";
-            URI[1]="http://dbpedia.org/resource/Forza_Italia";
-            URI[2]="http://dbpedia.org/resource/Denis_Verdini";
-            URI[3]="http://dbpedia.org/resource/Chamber_of_Deputies_(Italy)";
-            URI[4]="http://dbpedia.org/resource/Pier_Silvio_Berlusconi";
-        	Socket socket = new Socket("localhost", 1112);
+            String[] URI = new String[15753];
+	    
+	    URI[0]="http://dbpedia.org/resource/Bruce_Springsteen";
+            URI[1]="http://dbpedia.org/ontology/Song";
+
+            URI[0]="http://dbpedia.org/resource/Ingrid_Bergman";
+            URI[1]="Date";
+            //URI[2]="http://dbpedia.org/ontology/Person";
+	    //URI[4]="http://dbpedia.org/resource/Pier_Silvio_Berlusconi";
+        
+
+	    int i=0;
+	    Scanner in = new Scanner(new FileReader("/home_expes/dd77474h/Indexing-client/list"));
+	    //try {
+              while (in.hasNext()) {
+        	 URI[i]=in.nextLine();
+		 URI[i]=URI[i].substring(1, URI[i].length() - 1);
+		 System.out.println(URI[i]); 
+		 i++;
+	      }
+            //}
+
+
+            Socket socket = new Socket("localhost", 1112);
         	
-        	OutputStream os = socket.getOutputStream();
+            OutputStream os = socket.getOutputStream();
             ObjectOutputStream oos = new ObjectOutputStream(os);
             long startTime = System.currentTimeMillis();
             System.out.println("Sending ...");
@@ -30,11 +47,15 @@ public class Client2 {
             InputStream is = socket.getInputStream();
         	ObjectInputStream ois = new ObjectInputStream(is);
         	OpenMapRealMatrix to = (OpenMapRealMatrix)ois.readObject();
-        	
+        
+		FileOutputStream fout = new FileOutputStream("/home_expes/dd77474h/Indexing-client/matrix_similarity");
+        	ObjectOutputStream OOS = new ObjectOutputStream(fout);
+        	OOS.writeObject(to);
+	
         	long estimatedTime = System.currentTimeMillis() - startTime;
     		System.out.println(estimatedTime);
         	
-        	if (to!=null){
+        	/*if (to!=null){
         		for (int i1=0; i1<URI.length; i1++){
         			for (int j=0; j<URI.length; j++){
         				//if (to.)
@@ -42,7 +63,7 @@ public class Client2 {
         			}
         			System.out.print("\n");
         		}
-        	}
+        	}*/
         	
         	oos.close();
         	os.close();
